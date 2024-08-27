911d51e6dd657c97a2f44fd91aa8fa60312fc9e7
Alex <alex.maksuta@gmail.com>
2024-08-27 13:31:36 +0300

    Update Storage

--- include/Storage.hpp
+++ include/Storage.hpp
@@ -9,7 +9,9 @@ class Storage
 {
 public:
 	virtual std::optional<std::tuple<TimeSet, Data>> getUser(const User&, const Group&) = 0;
-	virtual std::vector<std::tuple<TimeSet, Data, User>> getAllUsersFromGroup(const Group&) = 0;
+	virtual std::vector<std::tuple<Data, User>> getAllUsersFromGroup(const Group&) = 0;
 	virtual void setValue(const User&, const Group&, const Data&) = 0;
+	virtual std::vector<std::tuple<Data, User>> getLimitTopUsersFromGroup(const Group&, std::size_t count) = 0;
+	virtual std::vector<std::tuple<Data, User, Group>> getLimitTopUsersFromAllGroup(std::size_t count) = 0;
 };
 
